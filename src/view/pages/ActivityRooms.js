25f03fc74b028731cc805aee6ef957cadfb9d5f1
fudgyfrog <jiekai98@gmail.com>
2022-09-27 16:05:41 +0800

    Update ActivityRooms.js
    destroy_example

--- src/view/pages/ActivityRooms.js
+++ src/view/pages/ActivityRooms.js
@@ -10,16 +10,7 @@ const ActivityRooms = ({setEventRoom}) => {
 
   useEffect(()=>{
     const q = query(collection(db, 'aRooms'),orderBy('cap','desc'))
-    let t=0
-    const unsubscribe = onSnapshot(q, (QuerySnapshot)=>{
-      let aRooms=[]
-      QuerySnapshot.forEach((doc)=>{
-        aRooms.push({...doc.data(),id:doc.id,timer:t*500})
-        t+=0.25;
-      })
-      console.log(aRooms);
-      setARooms(aRooms);
-    })
+    let t
   },[])
 
   
